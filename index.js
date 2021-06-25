7d1e94dfe146a83b17005e013a60675765561293
Zeynep Nida SARICAN <zsarican997@gmail.com>
2021-06-25 23:25:31 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var name = "Zeynep Nida SARICAN"
+var age = "23"
+
+
+document.getElementById("btn").addEventListener("click",function(){
+    document.getElementById("nameSurname").innerHTML=name,
+    document.getElementById("age").innerHTML=age
+    
+    })
